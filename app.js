103654ed7ceb3edfa94dce5e8bf9ea6e471a68ae
Andres1717 <mariaerq10@gmail.com>
2023-04-23 21:19:12 -0500

    Servidor funcionando, creada la base de datos y el modelo de usarios. No se ha creado ninguna ruta

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
-app.use(express.json())
+//SE CREA LA INSTANCIA DE LA APP
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-module.exports = app
+module.exports = app;
+
+//Dialect needs to be explicitly supplied as of v4.0.0
---
+++ server.js
@@ -0,0 +1,21 @@
+//SE IMPORTA DOTENV PARA ACCEDER A LAS VARIABLES DE ENTORNO
+//SE IMPORTA LA APP
+require('dotenv').config();
+const app = require('./app');
+const { db } = require('./database/config');
+
+//AUTENTICACION CON LA BASE DE DATOS
+db.authenticate()
+  .then(() => console.log('Database authenticated!'))
+  .catch((error) => console.log(error));
+
+//SINCRONIZACION CON LA BASE DE DATOS
+db.sync()
+  .then(() => console.log('Database synced!'))
+  .catch((error) => console.log(error));
+
+//DEFINIENDO EL PUERTO Y PONIENDO LA APP A ESCUCHAR
+const port = +process.env.PORT || 5600;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
