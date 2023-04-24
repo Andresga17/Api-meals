c189bf00754f45719264e94fa5a5f2a61599e8e7
Andres1717 <mariaerq10@gmail.com>
2023-04-24 16:24:20 -0500

    Creado el error handler

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,8 @@
-exports.createUser = async(req, res) => {}
-exports.loginUser= async(req, res) => {}
-exports.getAllOrdersByUser= async(req, res) => {}
-exports.getOrderById= async(req, res) => {}
-exports.UpdateUserProfile= async(req, res) => {}
-exports.deleteUserProfile= async(req, res) => {}
+const catchAsync = require("../../API_BLOG1/blog_api_gen_22/utils/catchAsync")
+
+exports.createUser = catchAsync(async(req, res) => {})
+exports.loginUser = catchAsync(async(req, res) => {})
+exports.getAllOrdersByUser = catchAsync(async(req, res) => {})
+exports.getOrderById = catchAsync(async(req, res) => {})
+exports.UpdateUserProfile = catchAsync(async(req, res) => {})
+exports.deleteUserProfile = catchAsync(async(req, res) => {})
